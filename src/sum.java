c73c58bbb8bfa01062e043877724307aacfc4224
Prajwal K N <prajwalkpura99@gmail.com>
2022-06-23 23:10:49 +0530

    Added Java

---
+++ src/sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.println("The sum of these numbers is");
+        int num1 = 6;
+        int num2 = 10;
+        int num3 = 25;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
