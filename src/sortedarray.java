c8476f8c3be68c91b14542e61d3f324a6c57fc58
Prajwal K N <prajwalkpura99@gmail.com>
2022-07-01 23:39:05 +0530

    Added Java

---
+++ src/sortedarray.java
@@ -0,0 +1,18 @@
+public class sortedarray {
+    public static void main(String[] args) {
+        boolean isSorted = true;
+        int [] arr = {1,12,3,5,79,24};
+        for(int i=0;i< arr.length-1;i++){
+            if (arr[i]>arr[i+1]){
+                isSorted = false;
+                break;
+            }
+        }
+        if(isSorted){
+            System.out.println("The array is sorted");
+        }
+        else {
+            System.out.println("The array is not sorted");
+        }
+    }
+}
